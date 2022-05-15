b68e209c109ee33a892f21e4ba30aa0577689297
Heejun Kim(samsung) <khj878@hanmail.net>
2022-05-15 18:54:07 +0900

    22.05.15. 커밋

--- 백준복습/11050 이항계수1.cpp
+++ 백준복습/11050 이항계수1.cpp
@@ -9,21 +9,11 @@ using namespace std;
 #define r(i, N) for(int i = 0; i < N; i++)
 #define rr(i, N) for(int i = 0; i <= N; i++)
 
-int N, K;
-vector<int> arr(5000005, 0);
-
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	cin >> N >> K;
-	r(i, N) {
-		cin >> arr[i];
-	}
-	sort(arr.begin(), arr.begin() + N);
-	cout << arr[K - 1];
 
 	return 0;
 }
--- 백준복습/1547. 공.cpp
+++ 백준복습/1547. 공.cpp
@@ -9,8 +9,8 @@ using namespace std;
 #define r(i, N) for(int i = 0; i < N; i++)
 #define rr(i, N) for(int i = 0; i <= N; i++)
 
-int N, K;
-vector<int> arr(5000005, 0);
+int N;
+vector<int> arr(3, 0);
 
 int main()
 {
@@ -18,12 +18,19 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> K;
+	arr[0] = 1;
+	cin >> N;
+	int x, y;
 	r(i, N) {
-		cin >> arr[i];
+		cin >> x >> y;
+		swap(arr[x - 1], arr[y - 1]);
 	}
-	sort(arr.begin(), arr.begin() + N);
-	cout << arr[K - 1];
+	for (int i = 0; i < 3; i++) {
+		if (arr[i] == 1) {
+			cout << i + 1;
+			break;
+		}
+	}
 
 	return 0;
 }
