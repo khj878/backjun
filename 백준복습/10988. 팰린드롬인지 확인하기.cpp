0f35444169bfe708367af21cab06382558809713
Heejun Kim(samsung) <khj878@hanmail.net>
2022-05-18 20:06:20 +0900

    22.05.18 커밋

--- 백준복습/10988. 팰린드롬인지 확인하기.cpp
+++ 백준복습/10988. 팰린드롬인지 확인하기.cpp
@@ -9,11 +9,24 @@ using namespace std;
 #define r(i, N) for(int i = 0; i < N; i++)
 #define rr(i, N) for(int i = 0; i <= N; i++)
 
+string S;
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
+	cin >> S;
+	bool flag = false;
+	r(i, S.length() / 2 - 1) {
+		if (S[i] != S[S.length() - i - 1]) {
+			flag = true;
+			break;
+		}
+	}
+	if (flag) cout << 0;
+	else cout << 1;
 
 	return 0;
 }
